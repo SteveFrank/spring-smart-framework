51da9ba40b654b9e4f647c747ac417aa239e8125
FrankQian <yangqian3@staff.weibo.com>
2020-12-31 17:07:48 +0800

    init

---
+++ smart-framework-web/src/main/java/com/smart/framwork/demo/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.smart.framwork.demo.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author yangqian
+ * @date 2020/12/31
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = "smart framework";
+        req.setAttribute("name", name);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+}
