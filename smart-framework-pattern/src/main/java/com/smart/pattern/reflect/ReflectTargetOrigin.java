7d6a86298d3972c731555411b59684c1f7729a2b
FrankQian <yangqian3@staff.weibo.com>
2021-01-01 21:28:31 +0800

    update -> 反射相关基础知识

---
+++ smart-framework-pattern/src/main/java/com/smart/pattern/reflect/ReflectTargetOrigin.java
@@ -0,0 +1,14 @@
+package com.smart.pattern.reflect;
+
+/**
+ * @author yangqian
+ * @date 2021/1/1
+ */
+public class ReflectTargetOrigin {
+
+    String defaultMember = "default";
+    public String publicMember = "public";
+    protected String protectedMember = "protected";
+    private String privateMember = "private";
+
+}
