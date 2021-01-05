e180c59dcde03d2c532f09be81235c09cea29f59
FrankQian <yangqian3@staff.weibo.com>
2021-01-05 08:59:54 +0800

    update -> 通过包名获取包下的class信息

--- smart-framework/src/main/java/org/smart/framwork/config/SmartConstants.java
+++ smart-framework/src/main/java/org/smart/framwork/config/SmartConstants.java
@@ -6,6 +6,9 @@ package org.smart.framwork.config;
  */
 public class SmartConstants {
 
+    /**
+     * 后续框架完善中可以考虑除了文件类型，还会有jar类型等
+     */
     public static final String FILE_PROTOCOL = "file";
 
     public static final String CLASS_SUFFIX = ".class";
