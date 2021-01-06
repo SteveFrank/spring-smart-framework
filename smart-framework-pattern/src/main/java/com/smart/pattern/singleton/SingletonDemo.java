b69da263922aa5b678180d95bec3110615736f17
FrankQian <yangqian3@staff.weibo.com>
2021-01-06 17:28:54 +0800

    update -> 更加安全的单例模式

--- smart-framework-pattern/src/main/java/com/smart/pattern/singleton/SingletonDemo.java
+++ smart-framework-pattern/src/main/java/com/smart/pattern/singleton/SingletonDemo.java
@@ -13,10 +13,11 @@ public class SingletonDemo {
     public static void main(String[] args) throws Exception {
         System.out.println(EnumStarvingSingleton.getInstance());
         Class clazz = EnumStarvingSingleton.class;
+        // 通过反射的方式强制获取
         Constructor constructor = clazz.getDeclaredConstructor();
         constructor.setAccessible(true);
-        EnumStarvingSingleton enumStarvingSingleton = (EnumStarvingSingleton)constructor.newInstance();
-        System.out.println(enumStarvingSingleton.getInstance());
+//        EnumStarvingSingleton enumStarvingSingleton = (EnumStarvingSingleton)constructor.newInstance();
+//        System.out.println(enumStarvingSingleton.getInstance());
     }
 
 }
